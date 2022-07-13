c2aa370f1717a433c409c3b54616bd37bcf54e34
Shiri Zadok <85099151+shirizadok1@users.noreply.github.com>
2022-07-13 13:26:04 +0300

    Create Header.jsx

---
+++ src/dkeeper_assets/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DoneAllIcon from "@material-ui/icons/DoneAll";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <DoneAllIcon />
+        Your Keeper App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
